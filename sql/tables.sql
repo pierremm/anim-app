70bac048aa08fb856653d36f1f1abfe51a745587
pierremm <pierremm@gmail.com>
2020-02-28 09:31:38 +0100

    Update tables.sql

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,80 +1,227 @@
-SET FOREIGN_KEY_CHECKS = 0;
-DROP TABLE IF EXISTS `personnes`;
-DROP TABLE IF EXISTS `qualites`;
-DROP TABLE IF EXISTS `lieux`;
-DROP TABLE IF EXISTS `etablissements`;
-DROP TABLE IF EXISTS `partenaires`;
-DROP TABLE IF EXISTS `projets`;
-DROP TABLE IF EXISTS `themes`;
-DROP TABLE IF EXISTS `publics`;
-SET FOREIGN_KEY_CHECKS = 1;
+-- phpMyAdmin SQL Dump
+-- version 4.9.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: uzn.myd.infomaniak.com
+-- Generation Time: Feb 28, 2020 at 09:29 AM
+-- Server version: 5.6.45-log
+-- PHP Version: 7.2.28
 
-CREATE TABLE `personnes` (
-    `id` INTEGER NOT NULL,
-    `nom` VARCHAR(50) NOT NULL,
-    `prenom` VARCHAR(50),
-    `tel` VARCHAR(20),
-    `email` VARCHAR(50),
-    `qualite` INTEGER,
-    PRIMARY KEY (`id`),
-    UNIQUE (`id`)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE `qualites` (
-    `id` INTEGER NOT NULL,
-    `nom` VARCHAR(50) NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE (`id`)
-);
 
-CREATE TABLE `lieux` (
-    `id` INTEGER NOT NULL,
-    `nom` VARCHAR(50) NOT NULL,
-    `adresse` VARCHAR(255),
-    `cp` INTEGER(10),
-    `ville` VARCHAR(50),
-    `contact` INTEGER,
-    PRIMARY KEY (`id`),
-    UNIQUE (`id`)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `uzn_app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `animations`
+--
+
+CREATE TABLE `animations` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `dateAnim` date DEFAULT NULL,
+  `theme` int(11) DEFAULT NULL,
+  `projet` int(11) DEFAULT NULL,
+  `partenaires` int(11) DEFAULT NULL,
+  `etablissement` int(11) DEFAULT NULL,
+  `lieu` int(11) DEFAULT NULL,
+  `public` int(11) DEFAULT NULL,
+  `effectif` int(11) DEFAULT NULL,
+  `demiJournees` int(11) DEFAULT NULL,
+  `matthias` int(11) DEFAULT NULL,
+  `noelie` int(11) DEFAULT NULL,
+  `benevoles` int(11) DEFAULT NULL,
+  `notes` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `etablissements`
+--
 
 CREATE TABLE `etablissements` (
-    `id` INTEGER NOT NULL,
-    `nom` VARCHAR(255) NOT NULL,
-    `tel` VARCHAR(20),
-    `email` VARCHAR(50),
-    `contact` INTEGER,
-    PRIMARY KEY (`id`),
-    UNIQUE (`id`)
-);
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `tel` varchar(20) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `contact` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lieux`
+--
+
+CREATE TABLE `lieux` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `adresse` varchar(255) DEFAULT NULL,
+  `cp` varchar(10) DEFAULT NULL,
+  `ville` varchar(50) DEFAULT NULL,
+  `contact` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `partenaires`
+--
 
 CREATE TABLE `partenaires` (
-    `id` INTEGER NOT NULL,
-    `nom` VARCHAR(50) NOT NULL,
-    `tel` VARCHAR(20),
-    `email` VARCHAR(50),
-    `contact` INTEGER(3),
-    PRIMARY KEY (`id`),
-    UNIQUE (`id`)
-);
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `tel` varchar(20) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `contact` int(3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projets`
+--
 
 CREATE TABLE `projets` (
-    `id` INTEGER NOT NULL,
-    `nom` VARCHAR(50) NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE (`id`)
-);
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `themes` (
-    `id` INTEGER NOT NULL,
-    `nom` VARCHAR(50) NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE (`id`)
-);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `publics`
+--
 
 CREATE TABLE `publics` (
-    `id` INTEGER NOT NULL,
-    `nom` VARCHAR(50) NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE (`id`)
-);
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `themes`
+--
+
+CREATE TABLE `themes` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `animations`
+--
+ALTER TABLE `animations`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `etablissements`
+--
+ALTER TABLE `etablissements`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `lieux`
+--
+ALTER TABLE `lieux`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `partenaires`
+--
+ALTER TABLE `partenaires`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `projets`
+--
+ALTER TABLE `projets`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `publics`
+--
+ALTER TABLE `publics`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `themes`
+--
+ALTER TABLE `themes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `animations`
+--
+ALTER TABLE `animations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `etablissements`
+--
+ALTER TABLE `etablissements`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `lieux`
+--
+ALTER TABLE `lieux`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `partenaires`
+--
+ALTER TABLE `partenaires`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `projets`
+--
+ALTER TABLE `projets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `publics`
+--
+ALTER TABLE `publics`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `themes`
+--
+ALTER TABLE `themes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
